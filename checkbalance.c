8e852d15dee8928e1f17b9cc423e6aaf652fd063
niten940 <nitenbajra2@gmail.com>
2025-02-06 18:59:37 +0545

    initial

---
+++ checkbalance.c
@@ -0,0 +1,34 @@
+//this is the branch to check balance 
+#include<stdio.h>
+
+void banking_login(){
+    FILE*fp;
+    char username[50];
+    fp=fopen("banking.txt","r");
+     if(fp==NULL)
+     {
+        printf("error opening \n try again");
+     }
+
+     printf("enter the username ");
+     scanf("%s",&username);
+
+     checkbalance(fp,username);
+
+     fclose(fp);
+}
+
+void checkbalance(FILE*fp,const char*username)
+{
+char storeduser_name[50];
+float balance;
+
+rewind(fp);
+while(fscanf(fp,"%s%f",storeduser_name,&balance)!=EOF){
+if(strcmp(storeduser_name,username)==0)
+{
+printf("your balance in this account is %.2f",balance);
+return;
+}}
+printf("user not found");
+}
